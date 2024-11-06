d5ad62d180ef1a0455060cb86bb19030fae7fecd
Sourabh Gour <saurabhgour2222@gmail.com>
2024-11-07 01:36:13 +0530

    Single Inheritance in Java Program added

---
+++ OOPS in Java/Inheritance/SingleInheritance/Derived.java
@@ -0,0 +1,28 @@
+class Base{
+
+void methodofBaseClass(){
+    System.out.println("Hey This is Base Class's method");
+}
+    
+    
+}
+
+class Derived extends Base{
+    
+
+    void methodofDerivedClass(){
+        System.out.println("Hey This is Derived Class's method");
+    }
+    
+    public static void main(String[] args) {
+        
+
+        Derived obj=new Derived();
+        obj.methodofBaseClass();
+        obj.methodofDerivedClass();
+    
+    
+    
+    }
+
+}
